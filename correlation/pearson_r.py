e944477c573f456cb177ccfa7412706b4a1398b4
FrontMan <isong.nathan90@gmail.com>
2022-10-06 23:41:04 +0100

    Edited pearson_r guided by unittest codes which helped pointed out the possible errors that could arise with data entry.

--- correlation/pearson_r.py
+++ correlation/pearson_r.py
@@ -46,18 +46,38 @@ def pearson_r(arg):
     inverse linear relation and 1 denotes strong direct linear
     relation.
     """
-    x, y = arg.keys()
-    if len(arg[x]) == len(arg[y]):
-        nth = len(arg[x])
-        summation_x, summation_y = sum_x_sum_y(arg).values()
-        numerator = nth * sum_xy(arg) - summation_x * summation_y
-        sum_squared_x, sum_squared_y = sum_squared_values(arg).values()
-        x_denominator_diff = nth * sum_squared_x - math.pow(summation_x, 2)
-        y_denominator_diff = nth * sum_squared_y - math.pow(summation_y, 2)
-        denominator = math.sqrt(x_denominator_diff * y_denominator_diff)
-        return numerator / denominator
-    return '''
-    Invalid data pair! check to confirm each dependent variable
-    has a correspondent independent variable. If you mean none 
-    use 0 instead of a missing data.
-    '''
+    try:
+        if type(arg) != dict:
+            raise TypeError('Invalid data format: Data must be dict and have only two variables')
+        elif len(arg.keys()) != 2:
+            raise ValueError('Invalid data format: Data must have only two variables')
+        else:
+            x, y = arg.keys()
+        for variable_values in arg.values():
+            for data in variable_values:
+                if type(data) != int and type(data) != float:
+                    raise TypeError('Check carefully, all data must be a real number!')
+        if not iter(arg[x]) or not iter(arg[y]):
+            raise TypeError('Not iterable: Data must be of the format {x: iterable, y: iterable}')
+        if len(arg[x]) == len(arg[y]):
+            nth = len(arg[x])
+            summation_x, summation_y = sum_x_sum_y(arg).values()
+            numerator = nth * sum_xy(arg) - summation_x * summation_y
+            sum_squared_x, sum_squared_y = sum_squared_values(arg).values()
+            x_denominator_diff = nth * sum_squared_x - math.pow(summation_x, 2)
+            y_denominator_diff = nth * sum_squared_y - math.pow(summation_y, 2)
+            denominator = math.sqrt(x_denominator_diff * y_denominator_diff)
+            return numerator / denominator
+        return '''
+        Invalid data pair! check to confirm each dependent variable
+        has a correspondent independent variable. If you mean none 
+        use 0 instead of a missing data.
+        '''
+    except TypeError as err:
+        if __name__ == '__main__':
+            print(err)
+        return err.args[0]
+    except ValueError as err:
+        if __name__ == '__main__':
+            print(err)
+        return err
--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -39,22 +39,21 @@ def winning_check(board):
         return True
     elif board[0][2] == board[1][1] == board[2][0]:
         return True
-    else:
-        return False
+    return False
 
 
 class T3Game:
     random_set = [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
 
-    def __init__(self, player_name, moves, board, score, my_play, win_check, player_id):
+    def __init__(self, player_name, moves, board, score, win_check, player_id, my_play_history=None, mode=None):
         self.player_name = player_name
         self.moves = moves
         self.board = board
         self.score = score
-        self.my_play = my_play
+        self.my_play_history = my_play_history
         self.win_check = win_check
         self.player_id = player_id
-        self.mode = 'hard'
+        self.mode = mode
 
     def movement(self):
         if self.player_name != 'Frontbot':
@@ -83,22 +82,28 @@ class T3Game:
             _intersect = list(item & set(my_list))
             if len(_intersect) == 1:
                 x, y = item - set(my_list)
-                if x in self.my_play and y in self.my_play:
+                if x in self.my_play_history and y in self.my_play_history:
                     self.moves.remove(_intersect[0])
-                    self.my_play.append(_intersect[0])
+                    self.my_play_history.append(_intersect[0])
                     return self.update_board(_intersect[0], self.board)
-                elif x not in self.my_play and y not in self.my_play:
+        for item in self.random_set:
+            _intersect = list(item & set(my_list))
+            if len(_intersect) == 1:
+                x, y = item - set(my_list)
+                if x not in self.my_play_history and y not in self.my_play_history:
                     self.moves.remove(_intersect[0])
-                    self.my_play.append(_intersect[0])
+                    self.my_play_history.append(_intersect[0])
                     return self.update_board(_intersect[0], self.board)
 
             elif len(_intersect) == 2:
-                y = item - set(my_list)
-                if y in self.my_play:
-                    player_number = random.choice(_intersect)
-                    self.moves.remove(player_number)
-                    self.my_play.append(player_number)
-                    return self.update_board(player_number, self.board)
+                x = item - set(my_list)
+                if x in self.my_play_history:
+                    y, z = _intersect
+                    if y in my_list and z in my_list:
+                        player_number = random.choice(_intersect)
+                        self.moves.remove(player_number)
+                        self.my_play_history.append(player_number)
+                        return self.update_board(player_number, self.board)
             else:
                 continue
         self.easy_mode()
@@ -106,7 +111,7 @@ class T3Game:
     def easy_mode(self):
         player_number = random.choice(self.moves)
         self.moves.remove(player_number)
-        self.my_play.append(player_number)
+        self.my_play_history.append(player_number)
         return self.update_board(player_number, self.board)
 
     def update_board(self, value, board):
@@ -138,6 +143,7 @@ while True:
 player2_name = 'Frontbot'
 record = {player1_name: 0, player2_name: 0, 'draw': 0}
 trial = True
+counter = random.choice([1, 0])
 while trial:
     template = [
         [1, 2, 3],
@@ -146,12 +152,14 @@ while trial:
     ]
 
     turn = 9
-    frontbot_play = []
+    frontbot_play_history = []
     my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    player1 = T3Game(player1_name, my_list, template, record, None, winning_check, player_id='X')
-    player2 = T3Game(player2_name, my_list, template, record, frontbot_play, winning_check, player_id='O')
+    player1 = T3Game(player1_name, my_list, template, record, winning_check, player_id='X')
+    player2 = T3Game(player2_name, my_list, template, record, winning_check, player_id='O',
+                     my_play_history=frontbot_play_history, mode='hard', )
     player = [player1, player2]
-    a = random.choice([1, 0])
+    counter += 1
+    a = counter % 2
     b = 1 - a
 
     while turn:
